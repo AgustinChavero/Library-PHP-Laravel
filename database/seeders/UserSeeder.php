97a6a29d577e029246c30f9c7a619c0d1c25364c
AgustinChavero <agustindanielchavero@gmail.com>
2024-01-13 01:23:11 -0300

    Seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'is_admin' => true,
+            'is_deleted' => false
+        ])
+    }
+}
