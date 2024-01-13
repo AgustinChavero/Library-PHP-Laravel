e80c2dcd957f8c3b7b1505b3d57fcb166ec25809
AgustinChavero <agustindanielchavero@gmail.com>
2024-01-13 01:22:56 -0300

    Models & Relations

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,8 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $primaryKey = 'id';
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = $model->{$model->getKeyName()} ?: (string) Str::uuid();
+        });
+    }
+
+    @var array
+    protected $fillable = [
+        'id',
+        'review_text',
+        'rating',
+        'user_id',
+        'book_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+        # return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function books(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+        # return $this->belongsTo(Book::class, 'book_id', 'id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,17 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Str;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-     
+    
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $primaryKey = 'id';
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = $model->{$model->getKeyName()} ?: (string) Str::uuid();
+        });
+    }
+
     protected $fillable = [
+        'id',
         'name',
         'email',
         'password',
+        'is_admin',
+        'is_deleted'
     ];
 
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+        # return $this->hasMany(Review::class, 'user_id', 'id');
+    }
 }
