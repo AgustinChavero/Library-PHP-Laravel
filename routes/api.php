8e96134fd481b54574f218865fadc47b1f0522e6
AgustinChavero <agustindanielchavero@gmail.com>
2024-01-13 01:20:42 -0300

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/user', [UserController::class, 'getAll'])->name('user.get.all');
+Route::post('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::get('/user/detail/{id?}', [UserController::class, 'getOne'])->name('user.get.one');
+Route::put('/user/edit/{id?}', [UserController::class, 'update'])->name('user.update');
+Route::delete('/user/delete/{id?}', [UserController::class, 'delete'])->name('user.delete');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\BookController;
 
-
-Route::post('/user/create', [UserController::class, 'create'])->name('user.create');
-Route::get('/user', [UserController::class, 'getAll'])->name('user.get.all');
-Route::get('/user/detail/{id?}', [UserController::class, 'getOne'])->name('user.get.one');
-Route::put('/user/edit/{id?}', [UserController::class, 'edit'])->name('user.edit');
+Route::get('/', function () {
+    return view('welcome');
+});
